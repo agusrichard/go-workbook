fdeaac85e0c0e722bbc30505981d4fc615c98a6e
Agus Richard <agus.richard21@gmail.com>
2021-02-04 01:22:47 +0700

    Initializing todo service

---
+++ grpc-todo/config/database.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+var DB *sqlx.DB
+
+// ConnectDB to get all needed db connections for application
+func ConnectDB() *sqlx.DB {
+	DB = getDBConnection()
+
+	return DB
+}
+
+func getDBConnection() *sqlx.DB {
+	var dbConnectionStr string
+
+	dbConnectionStr = fmt.Sprintf(
+		"host=%s port=%d dbname=%s user=%s password=%s sslmode=disable",
+		"localhost",
+		5432,
+		"grpc_auth",
+		"postgres",
+		"postgres",
+	)
+
+	db, err := sqlx.Open("postgres", dbConnectionStr)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	//TODO: experiment with correct values
+	db.SetMaxIdleConns(1)
+	db.SetMaxOpenConns(5)
+
+	return db
+}
---
+++ grpc-todo/go.mod
@@ -0,0 +1,8 @@
+module grpc-todo
+
+go 1.15
+
+require (
+	github.com/jmoiron/sqlx v1.3.1 // indirect
+	github.com/lib/pq v1.9.0 // indirect
+)
