cf6814fb42a2b6c3d3c7799ba550c73db86189bd
agusrichard <agus.richard21@gmail.com>
2021-05-24 07:29:03 +0700

    rename folder

---
+++ restapi-test-app/go.mod
@@ -0,0 +1,3 @@
+module restapi-tested-app
+
+go 1.15
---
+++ restapi-test-app/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"restapi-tested-app/example"
+)
+
+func main() {
+	fmt.Println("Saskia Nurul Azhima")
+	example.ExampleFunction()
+}
