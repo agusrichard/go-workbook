ea62f2c19b53d4e6f01f6bdd27eaeb5f533fd57a
agusrichard <agus.richard21@gmail.com>
2021-06-03 01:56:33 +0700

    it works i suppose

--- play-with-reflect/main.go
+++ play-with-reflect/main.go
@@ -2,104 +2,86 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"reflect"
 	"time"
 )
 
-type TodoBase struct {
-	ID        int       `json:"id" db:"id"`
-	Username  string    `json:"username" db:"username"`
-	Title     string    `json:"title" db:"title"`
-	CreatedAt time.Time `json:"createdAt" db:"created_at"`
-}
-
 type Todo struct {
-	TodoBase
+	ID          int            `json:"id" db:"id"`
+	Username    string         `json:"username" db:"username"`
+	Title       string         `json:"title" db:"title"`
+	CreatedAt   time.Time      `json:"createdAt" db:"created_at"`
 	Description sql.NullString `json:"description" db:"description"`
 	ModifiedAt  sql.NullTime   `json:"modifiedAt" db:"modified_at"`
 }
 
-type TodoConcrete struct {
-	TodoBase
-	Description string    `json:"description" db:"description"`
-	ModifiedAt  time.Time `json:"modifiedAt" db:"modified_at"`
+func (t *Todo) ToNotNullable() interface{} {
+	return InitilizeNotNullStruct(t)
 }
 
-func (t *Todo) IsValid() bool {
-	return t.Username != "" || t.Title != ""
-}
+func InitilizeNotNullStruct(nullableStruct interface{}) interface{} {
+	fmt.Println("InitilizeNotNullStruct")
+	var sfs []reflect.StructField
 
-func (t *Todo) ToConcrete() TodoConcrete {
-	return ToConcrete(t, TodoConcrete{}, TodoBase{}).(TodoConcrete)
-}
+	nst := reflect.TypeOf(nullableStruct).Elem()
+	nsv := reflect.ValueOf(nullableStruct).Elem()
 
-func ToConcrete(abstractPointer interface{}, emptyConcretePointer interface{}, baseStruct interface{}) interface{} {
-	tVal := reflect.ValueOf(abstractPointer).Elem()
-	cVal := reflect.New(reflect.TypeOf(emptyConcretePointer))
+	fmt.Println(sfs)
 
-	numOfFields := cVal.Elem().NumField()
-	for i := 0; i < numOfFields; i++ {
-		if tVal.Field(i).Type() == reflect.TypeOf(baseStruct) {
-			cVal.Elem().Field(i).Set(tVal.Field(i))
-			continue
+	numFields := nsv.NumField()
+	for i := 0; i < numFields; i++ {
+		var t reflect.Type
+
+		switch reflect.TypeOf(nst.Field(i)) {
+		case reflect.TypeOf(sql.NullString{}):
+			t = reflect.TypeOf("")
+		case reflect.TypeOf(sql.NullBool{}):
+			t = reflect.TypeOf(true)
+		case reflect.TypeOf(sql.NullFloat64{}):
+			t = reflect.TypeOf("0.1")
+		case reflect.TypeOf(sql.NullInt64{}):
+			t = reflect.TypeOf("0")
+		case reflect.TypeOf(sql.NullTime{}):
+			t = reflect.TypeOf(time.Now())
+		default:
+			t = nst.Field(i).Type
 		}
-
-		if tVal.Field(i).Type() == reflect.TypeOf(sql.NullString{}) {
-			cVal.Elem().Field(i).SetString(tVal.Field(i).Interface().(sql.NullString).String)
-			continue
-		}
-
-		if tVal.Field(i).Type() == reflect.TypeOf(sql.NullBool{}) {
-			cVal.Elem().Field(i).SetBool(tVal.Field(i).Interface().(sql.NullBool).Bool)
-			continue
-		}
-
-		if tVal.Field(i).Type() == reflect.TypeOf(sql.NullFloat64{}) {
-			cVal.Elem().Field(i).SetFloat(tVal.Field(i).Interface().(sql.NullFloat64).Float64)
-			continue
-		}
-
-		if tVal.Field(i).Type() == reflect.TypeOf(sql.NullInt64{}) {
-			cVal.Elem().Field(i).SetInt(tVal.Field(i).Interface().(sql.NullInt64).Int64)
-			continue
-		}
-
-		if tVal.Field(i).Type() == reflect.TypeOf(sql.NullTime{}) {
-			cVal.Elem().Field(i).Set(reflect.ValueOf(tVal.Field(i).Interface().(sql.NullTime).Time))
-			continue
-		}
+		sfs = append(sfs, reflect.StructField{
+			Name: nst.Field(i).Name,
+			Type: t,
+			Tag:  nst.Field(i).Tag,
+		})
 	}
 
-	return cVal.Elem().Interface()
+	fmt.Println(sfs)
+
+	return nil
 }
 
 func main() {
 	todo := Todo{
-		TodoBase: TodoBase{
-			ID:       1,
-			Username: "sekardayu",
-			Title:    "title",
-			CreatedAt: time.Now(),
-		},
+		ID:        1,
+		Username:  "sekardayu",
+		Title:     "title",
+		CreatedAt: time.Now(),
 		Description: sql.NullString{
 			String: "description",
 			Valid:  true,
 		},
 		ModifiedAt: sql.NullTime{
-			Time: time.Now(),
+			Time:  time.Now(),
 			Valid: true,
 		},
 	}
 	fmt.Println(todo)
-	cTodo := todo.ToConcrete()
+	cTodo := todo.ToNotNullable()
 	fmt.Println(cTodo)
 
-	result, err := json.Marshal(cTodo)
-	if err != nil {
-		panic(err)
-	}
-
-	println(string(result))
+	//result, err := json.Marshal(cTodo)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//println(string(result))
 }
