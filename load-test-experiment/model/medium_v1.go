0eb9e4c99971acdab7261ca6c84749cbf9e6ee09
agusrichard <agus.richard21@gmail.com>
2021-06-13 11:25:18 +0700

    finish working medium version 1

--- load-test-experiment/model/medium_v1.go
+++ load-test-experiment/model/medium_v1.go
@@ -34,29 +34,29 @@ type MediumV1Model struct {
 }
 
 type MediumV1SmallShape struct {
-	ID            int       `json:"id" db:"id"`
-	FieldOne      string    `json:"fieldOne" db:"field_one"`
-	FieldTwo      float64   `json:"fieldTwo" db:"field_two"`
-	FieldThree    string    `json:"fieldThree" db:"field_three"`
-	FieldFour     string `json:"fieldFour" db:"field_four"`
-	SmallLargeKey int       `json:"smallLargeKey" db:"small_large_key"`
+	ID            int     `json:"id" db:"id"`
+	FieldOne      string  `json:"fieldOne" db:"field_one"`
+	FieldTwo      float64 `json:"fieldTwo" db:"field_two"`
+	FieldThree    string  `json:"fieldThree" db:"field_three"`
+	FieldFour     string  `json:"fieldFour" db:"field_four"`
+	SmallLargeKey int     `json:"smallLargeKey" db:"small_large_key"`
 }
 
 type MediumV1Shape struct {
 	ID                   int                  `json:"id" db:"id"`
 	FieldOne             string               `json:"fieldOne" db:"field_one"`
 	FieldTwo             float64              `json:"fieldTwo" db:"field_two"`
-	FieldThree           string           `json:"fieldThree" db:"field_three"`
+	FieldThree           string               `json:"fieldThree" db:"field_three"`
 	FieldFour            time.Time            `json:"fieldFour" db:"field_four"`
 	FieldFive            int                  `json:"FieldFive" db:"field_five"`
 	FieldSix             string               `json:"fieldSix" db:"field_six"`
 	FieldSeven           float64              `json:"fieldSeven" db:"field_seven"`
-	FieldEight           string           `json:"fieldEight" db:"field_eight"`
+	FieldEight           string               `json:"fieldEight" db:"field_eight"`
 	FieldNine            time.Time            `json:"fieldNine" db:"field_nine"`
 	FieldTen             int                  `json:"fieldTen" db:"field_ten"`
 	FieldEleven          string               `json:"fieldEleven" db:"field_eleven"`
 	FieldTwelve          float64              `json:"fieldTwelve" db:"field_twelve"`
-	FieldThirteen        string           `json:"fieldThirteen" db:"field_thirteen"`
+	FieldThirteen        string               `json:"fieldThirteen" db:"field_thirteen"`
 	FieldFourteen        time.Time            `json:"fieldFourteen" db:"field_fourteen"`
 	MediumSmallModelList []MediumV1SmallShape `json:"mediumSmallModelList"`
 }
